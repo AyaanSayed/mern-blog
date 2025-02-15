860abf1b465867426713e6f7b32da99467dc996c
AyaanSayed <138655396+AyaanSayed@users.noreply.github.com>
2025-02-16 00:21:45 +0530

    added create a post api route

--- api/index.js
+++ api/index.js
@@ -5,8 +5,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
+import postRoute from './routes/post.route.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +26,8 @@ mongoose.connect(process.env.MONGO)
 app.use("/api/user", userRoute);
 
 app.use("/api/auth", authRoute);
+
+app.use("/api/post", postRoute);
 
 
 
