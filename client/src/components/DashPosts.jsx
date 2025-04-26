66015c363635aae081a0d2b47e7a0aa8b2bbeb4d
AyaanSayed <138655396+AyaanSayed@users.noreply.github.com>
2025-04-26 15:57:06 +0530

    added show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,4 +1,4 @@
-import { Table } from 'flowbite-react';
+import { Button, Table } from 'flowbite-react';
  import { useEffect, useState } from 'react';
  import { useSelector } from 'react-redux';
  import { Link } from 'react-router-dom';
@@ -6,6 +6,7 @@ import { Table } from 'flowbite-react';
  export default function DashPosts() {
    const { currentUser } = useSelector((state) => state.user);
    const [userPosts, setUserPosts] = useState([]);
+   const [showMore, setShowMore] = useState(true);
    console.log(userPosts);
    useEffect(() => {
      const fetchPosts = async () => {
@@ -14,7 +15,11 @@ import { Table } from 'flowbite-react';
          const data = await res.json();
          if (res.ok) {
            setUserPosts(data.posts);
-         }
+           if (data.posts.length < 5) {
+              setShowMore(false);
+            } 
+           }
+         
        } catch (error) {
          console.log(error.message);
        }
@@ -24,6 +29,20 @@ import { Table } from 'flowbite-react';
      }
    }, [currentUser._id]);
 
+   const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    const res = await fetch(
+      `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}` 
+    );
+    const data = await res.json();
+    if (res.ok) {
+      setUserPosts((prev) => [...prev, ...data.posts]);
+      if (data.posts.length < 9) {
+        setShowMore(false);
+      }
+
+   }
+  }
    return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
     {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -80,6 +99,11 @@ import { Table } from 'flowbite-react';
             </Table.Body>
           ))}
         </Table>
+        {showMore && (
+          <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+            Show more
+          </button>
+        )}
       </>
     ) : (
       <p>You have no posts yet!</p>
